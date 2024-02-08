a0c7ca2ee8a3718a9fca303c63752ce31754edd5
Tuan Hoang <frditc@gmail.com>
2024-02-08 10:25:44 +0700

    Update module-plts.c

--- arch/arm64/kernel/module-plts.c
+++ arch/arm64/kernel/module-plts.c
@@ -7,6 +7,7 @@
  */
 #include <asm-generic/asm-offsets.h>
 #include <arch/arm64/include/asm/insn.h>
+#include <arch/powerpc/boot/stdint.h>
 extern int read_cpu_memory(void *addr, size_t size);
 static int aarch64_insn_decode_register(int regtype, unsigned long addr)
 {
